c37f39b73ff44d5b25884383015da0927a14e463
Bart <bart00n2@hotmail.com>
2011-09-25 16:37:14 +0200

    Gestion des arguments + mode verbose + stub pour le Vertex Recolor

--- VertexRecolor/main.cpp
+++ VertexRecolor/main.cpp
@@ -1,26 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mesh_file_helper.h"
-
+#include "vertex_recolor.h"
+	
 
 // ============================================================================
 // ----------------------------------------------------------------------------
 // ============================================================================
 
+void Usage(const std::string& parExecFilename)
+{
+	printf("Usage: %s [-v] <COLORED_FILE> <TRIANGLE_FILE> <OUTPUT_FILE>\n", parExecFilename.c_str());
+	exit(1);
+}
+
+// ----------------------------------------------------------------------------
+
 int main(int argc, char* argv[])
 {
-	if (argc != 4)
+	if (argc < 4 || argc > 5)
+		Usage(argv[0]);
+
+	bool verbose = false;
+	int argOffset = 0;
+	if (argc == 5)
 	{
-		printf("Usage: %s <COLORED_FILE> <TRIANGLE_FILE> <OUTPUT_FILE>\n", argv[0]);
-		exit(1);
+		if (strncmp("-v", argv[1], 2) != 0)
+			Usage(argv[0]);
+		verbose = true;
+		argOffset = 1;
+	}
+	else if ((strncmp("-v", argv[1], 2) == 0) ||
+			(strncmp("-v", argv[2], 2) == 0) ||
+			(strncmp("-v", argv[3], 2) == 0))
+	{
+		Usage(argv[0]);
+	}
+	
+	if (verbose)
+	{
+		printf("=== VERTEX RECOLOR ===\n");
+		printf("[+] Loading meshes...\n");
 	}
 
-	const Mesh* coloredMesh = MeshFileHelper::LoadMeshFromAsciiPlyFileWithPosNormCol(argv[1]);
-	const Mesh* triMesh = MeshFileHelper::LoadMeshFromBinaryPlyFileWithPosAndTri(argv[2]);
+	const Mesh* coloredMesh = MeshFileHelper::LoadMeshFromAsciiPlyFileWithPosNormCol(argv[1 + argOffset]);
+	if (!coloredMesh)
+	{
+		if (verbose)
+			printf("[-] Failed to load colored mesh '%s'\n", argv[1 + argOffset]);
+		exit(2);
+	}
+	else if (verbose)
+		printf("[+] Colored Mesh loaded\n");
 
-	const Mesh* finalMesh = 0; // FIXME
+	const Mesh* triMesh = MeshFileHelper::LoadMeshFromBinaryPlyFileWithPosAndTri(argv[2 + argOffset]);
+	if (!triMesh)
+	{
+		if (verbose)
+			printf("[-] Failed to load triangularized mesh '%s'\n", argv[2 + argOffset]);
+		exit(3);
+	}
+	else if (verbose)
+		printf("[+] Triangularized Mesh loaded\n");
 
-	MeshFileHelper::SaveMeshToBinaryPlyFile(*finalMesh, argv[3]);
+	if (verbose)
+		printf("[+] Recovering color...\n");
+	Mesh* finalMesh = VertexRecolor::RecoverColor(*coloredMesh, *triMesh, verbose);
+	if (!finalMesh)
+	{
+		if (verbose)
+			printf("[-] Failed to recover color\n");
+		exit(4);
+	}
+	else if (verbose)
+		printf("[+] Color recovered\n");
+
+	MeshFileHelper::SaveMeshToBinaryPlyFile(*finalMesh, argv[3 + argOffset]);
+	if (verbose)
+		printf("[+] Colored & Triangularized mesh is saved\n");
 
 	return 0;
 }
