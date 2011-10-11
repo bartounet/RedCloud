818777cbde991a61fe2064d342e58b7161041be2
Bart <bart00n2@hotmail.com>
2011-10-11 17:37:00 +0200

    Stub pour le Quadrics based Mesh Simplifier

---
+++ QuadricsBasedMeshSimplifier/main.cpp
@@ -0,0 +1,14 @@
+// Quadrics based mesh simplifier
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char **argv)
+{
+	printf("Quadrics based mesh simplifier\n");
+
+	return 0;
+}
