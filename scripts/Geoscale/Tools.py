8321b140ce5d69bb942b4595937968f9e67418bd
Thibaut <t.munzer@gmail.com>
2012-03-27 01:09:58 +0200

    Add copyrights

--- scripts/Geoscale/Tools.py
+++ scripts/Geoscale/Tools.py
@@ -1,3 +1,23 @@
+#     This file is part of Geoscale tool (see http://www.geoscale.org/).
+# 
+#     Copyright (C) 2011 O.R.A., Jonathan Cloux (head developer), Benoît Millot
+#     Original idea from O.R.A. by Adrien Normier
+#     Contacts : ora.ctc@gmail.com (+336 77 09 31 16)
+#                jonathanclx@gmail.com   
+# 
+#     Geoscale is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     Geoscale is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with Geoscale.  If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import math
 
--- src/Gui/uploadwidget.cpp
+++ src/Gui/uploadwidget.cpp
@@ -1,4 +1,24 @@
-	#include "uploadwidget.h"
+//     This file is part of RedCloud tool.
+// 
+//     Copyright (C) 2012 Munzer Thibaut, Jonathan Aigrain, Adrien Chey, Nicolas Fabretti
+//     Original idea from O.R.A. by Adrien Normier
+//     Contacts : ora.ctc@gmail.com (+336 77 09 31 16)
+//                redcloud.contact@gmail.com   
+// 
+//     Redcloud is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     Geoscale is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with Geoscale.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "uploadwidget.h"
 #include <QFileDialog>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
